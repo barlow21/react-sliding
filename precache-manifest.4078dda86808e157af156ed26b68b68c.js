05e03963dea9875362392a6b58999de4342a2fc5
barlow21 <gerrit.boeing@gmx.de>
2019-04-25 18:21:15 +0200

    Updates

---
+++ precache-manifest.4078dda86808e157af156ed26b68b68c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01baad35c0901f411a4c",
+    "url": "/react-sliding/static/js/runtime~main.7042306e.js"
+  },
+  {
+    "revision": "4e20a3bec42cfd3fbeba",
+    "url": "/react-sliding/static/js/main.91cae7ef.chunk.js"
+  },
+  {
+    "revision": "118d9e8164459baf1876",
+    "url": "/react-sliding/static/js/2.59fed3ab.chunk.js"
+  },
+  {
+    "revision": "4e20a3bec42cfd3fbeba",
+    "url": "/react-sliding/static/css/main.d81022f5.chunk.css"
+  },
+  {
+    "revision": "f75817f7c228b7259bacd1ddacc3ce07",
+    "url": "/react-sliding/index.html"
+  }
+];
